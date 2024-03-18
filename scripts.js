e4acd2741752fad8551533595ff9cfc815e00a83
JamesMacin <162409568+JamesMacin@users.noreply.github.com>
2024-03-17 20:54:05 -0600

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var hamburger = document.querySelector('.hamburger-menu');
-    var menuList = document.querySelector('.menu-list');
-
-    hamburger.addEventListener('click', function () {
-        menuList.classList.toggle('show');
-    });
-});
